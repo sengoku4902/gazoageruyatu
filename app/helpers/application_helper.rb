b15f401cec628b3345c6c33cdf0d98cea42f1f6e
sengoku4902 <sasazaki4902@gmail.com>
2020-12-16 11:52:16 +0900

    1216

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def avatar_url(user)
+        return user.profile_photo unless user.profile_photo.nil?
+        gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+        "https://icon1216.s3-ap-northeast-1.amazonaws.com/gazoageruyatu/icon.png"
+    end
 end
