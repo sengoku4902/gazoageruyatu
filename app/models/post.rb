c76f0f053876ccef6baf52537f393c3ccfe69977
sengoku4902 <sasazaki4902@gmail.com>
2020-12-18 10:31:52 +0900

    1218

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
   has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :photos
-  def liked_by(current_user))
+  def liked_by(current_user)
     Like.find_by(user_id: current_user.id, post_id: id)
   end
 end
